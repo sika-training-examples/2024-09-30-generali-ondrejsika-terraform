1000242b3bcc46bcac4439351e7c9bba96f25d36
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-30 14:52:09 +0200

    feat: Creare hello_world_webapp

--- main.tf
+++ main.tf
@@ -19,3 +19,18 @@ module "service_plan" {
   name     = "ondrejsika"
   sku_name = "P1v2"
 }
+
+module "hello_world_webapp" {
+  source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//webapp?ref=webapp"
+
+  name                = "example-generali-ondrejsika-hello-world"
+  resource_group_name = module.service_plan.resource_group_name
+  location            = module.service_plan.resource_group_location
+  service_plan_id     = module.service_plan.service_plan_id
+  docker_image_name   = "ondrejsika/training-example"
+  docker_registry_url = "https://docker.io"
+}
+
+output "hello_world_url" {
+  value = "https://${module.hello_world_webapp.default_hostname}"
+}
