375cff97bf714649994e17db7fbb724abbf06d0f
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-30 11:30:18 +0200

    feat: Create subnet

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "example" {
   name     = "example-generali-ondrejsika"
   location = "westeurope"
 }
+
+resource "azurerm_subnet" "example" {
+  name                 = "ondjrejsika"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = "example-generali-network"
+  address_prefixes     = ["10.0.1.0/24"]
+}
