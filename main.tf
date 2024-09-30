d679c25d7468c0bd7dfa5cf6b345156fdc59c60b
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-30 14:27:27 +0200

    feat: Create service_plan

--- main.tf
+++ main.tf
@@ -12,3 +12,10 @@ data "azurerm_subnet" "ondrejsika" {
 output "subnet_id" {
   value = data.azurerm_subnet.ondrejsika.id
 }
+
+module "service_plan" {
+  source = "git::https://gitlab.sikademo.com/generali/generali-terraform-modules.git//service_plan?ref=webapp"
+
+  name     = "ondrejsika"
+  sku_name = "P1v2"
+}
